e1d3de91f07c01fc822de7864a130627fb7229d8
EuricoCruz <euricoscruz@hotmail.com>
2021-09-13 08:47:17 -0300

    add new functions

--- cadastro/rotas.js
+++ cadastro/rotas.js
@@ -1,13 +1,25 @@
+const pgPool = require('../data/index-pg'); 
+
 module.exports = app => {
   app
     .route('/cadastro')
     .get((req, res) => {
 			res.send('Olá, tela de cadastro!')
 		})
-		.post((req, res) => {
-			const nome = req.body.nome
-			const contato = req.body.contato 
-			console.log({nome, contato })
-			res.send('requisição aqui')
+		.post(async (req, res) => {		
+			let sql = 'INSERT INTO contacts(nome,celular) VALUES ($1,$2)';
+			let nome = 'eurico';
+			let celular = '5541990355873';
+      let values = [nome,celular]
+
+			try {
+				const resultado = await pgPool.query(sql, values)
+				console.log(resultado.rows[0])
+				res.status(201).json('deu certo')
+
+			} catch (err) {
+				console.log(err.stack)
+			}
 		})
+
 };
