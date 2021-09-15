ee98f47ff888db17d329b1aac32470151a98b454
EuricoCruz <euricoscruz@hotmail.com>
2021-09-15 08:20:30 -0300

    finisehd login requisitions

--- usuarios/rotas.js
+++ usuarios/rotas.js
@@ -1,19 +1,18 @@
 const UsuariosControlador = require('./usuarios-controlador');
-const UsuarioDaos = require('./usuario-daos')
+const UsuarioDaos = require('./usuario-daos');
+const middlewaresAutenticacao = require('./middlewares-autenticacao');
 
 module.exports = app => {
 	app
 		.route('/user/seed')
 		.post(async (req, res) => {
-				await UsuarioDaos.AlimentaBaseDeUsuarios(res)
+				await UsuarioDaos.alimentaBaseDeUsuarios(res);
 		})	
   app
     .route('/login')
     .get((req, res) => {
-			res.send('Olá, tela de login!')
+			res.send('Olá, tela de login!');
 		})
-		.post(async (req, res) => {
-				const { nome, senha } = req.body;
-				UsuarioDaos.buscaPorNome(nome, senha, res)
-		})
+		.post(middlewaresAutenticacao.local,
+					UsuariosControlador.login)
 };
--- usuarios/usuarios-controlador.js
+++ usuarios/usuarios-controlador.js
@@ -1,21 +1,28 @@
+const UsuarioDaos = require('./usuario-daos');
 const jwt = require('jsonwebtoken')
 
 function criaTokenJWT(usuario) {
   const payload = {
     id: usuario.id 
-  }
+  };
 
   const token = jwt.sign(payload, process.env.CHAVE_JWT, {expiresIn: '15m'})
 
   return token;
 
-}
+};
 
 
 module.exports = {
-  login: (req, res) => {
-    const token = criaTokenJWT(req.user);
-    res.set('Authorization', token)
-    res.status(204).send();
+  login: async (req, res) => {
+    try {
+      const user = req.user;
+      const token = criaTokenJWT(req.user);
+      res.set('Authorization', token);
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err)
+    }
+
   },
-}
+};
